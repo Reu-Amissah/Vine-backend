0a69a07a52a9cc9b7188ce4d0eb0d66ea26032f7
Reu-Amissah <reujoyamissah@gmail.com>
2023-02-18 23:49:18 +0000

    adding api Serializers, making a POST request

--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from contact.models import Item
+from .serializers import ItemSerializer
 
 @api_view(['GET'])
 def getData (request):
-    person = {'name': 'George', 'age': 35}
-    return Response(person)
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)
+    return Response(serializer.data)
