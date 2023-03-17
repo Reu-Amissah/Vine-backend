b7645ade6eb55d8966c497e74a90963f9d112dba
Reu-Amissah <reujoyamissah@gmail.com>
2023-03-17 23:35:15 +0000

    fix some url paths

--- vine-backend/urls.py
+++ vine-backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contacts', include ('contact.urls')),
+    path('', include ('contact.urls')),
 ]
